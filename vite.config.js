c34a4d0537d86ff980efe93a1def5fdf36fa84f2
abhit <sahuabhit27@gmail.com>
2025-03-08 22:25:08 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
-})
+  optimizeDeps: {
+    include: ["framer-motion", "lucide-react"],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
